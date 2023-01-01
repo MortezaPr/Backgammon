c1971cc2d2239c219c917381d80f02b1594233c8
Morteza Pourramzan <morteza9427@gmail.com>
2023-01-01 18:19:15 +0330

    updated checkState function

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,15 @@ function App() {
     turn: 1,
   });
 
+  const [white, setWhite] = useState({
+    moves: [],
+  });
+
+  const [black, setBlack] = useState({
+    moves: [],
+  });
+
+  // check game's states
   useEffect(() => {
     if (game.dice.length == 2) {
       toast(game.dice[0]);
@@ -22,6 +31,21 @@ function App() {
     }
   }, [game.dice]);
 
+  // check white player's states
+  useEffect(() => {
+    if (white.moves.length == 2) {
+      checkState(white.moves[0], white.moves[1]);
+    }
+  }, [white.moves]);
+
+  // check black player's states
+  useEffect(() => {
+    if (black.moves.length == 2) {
+      checkState(black.moves[0], black.moves[1]);
+    }
+  }, [black.moves]);
+
+  // roll dice function, it will generate two random numbers between 1 and 6
   function rollDice() {
     const first = Math.floor(Math.random() * 6) + 1;
     const second = Math.floor(Math.random() * 6) + 1;
@@ -31,14 +55,40 @@ function App() {
   }
 
   function checkState(from, to) {
-    const currBoard = [...board];
+    let player = white;
+    if (game.turn == -1) {
+      player = black;
+    }
+    let temp = { ...game };
+    let thePiece = temp.board[from].pop();
+    temp.board[to].push(thePiece);
+    setGame(temp);
+    player.moves = [];
 
-    // TODO
-
-    setBoard(currBoard);
+    // const currBoard = [...board];
+    // // TODO
+    // setBoard(currBoard);
   }
 
-  function select(index) {}
+  // select bar function, it will select the bar player clicked on
+  function select(index) {
+    let player = white;
+    if (game.turn == -1) {
+      player = black;
+    }
+    // check if the first selected bar is empty
+    if (game.board[index].top() == undefined && player.moves.length == 0) {
+      toast.error("There Is No Piece In This Place");
+    } else {
+      let temp = { ...player };
+      temp.moves = [...player.moves, index];
+      if (player == white) {
+        setWhite(temp);
+      } else {
+        setBlack(temp);
+      }
+    }
+  }
 
   return (
     <>
