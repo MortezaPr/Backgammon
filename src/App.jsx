b9f86dcaf0aa5dc132db38dd0a5c8984b6f46894
Morteza Pourramzan <morteza9427@gmail.com>
2023-01-02 22:01:55 +0330

    updated movement

--- src/App.jsx
+++ src/App.jsx
@@ -9,91 +9,141 @@ import { initialState } from "./logic";
 // https://salamdonya.com/fun/how-to-play-backgammon
 
 function App() {
-  const [game, setGame] = useState({
-    board: initialState,
-    dice: [],
-    turn: 1,
+  const [dice, setDice] = useState([]);
+  const [board, setBoard] = useState(initialState);
+  const [turn, setTurn] = useState(1);
+
+  const [player, setPlayer] = useState({
+    selectedBars: [],
+    availableMoves: [],
   });
-
-  const [white, setWhite] = useState({
-    moves: [],
-  });
-
-  const [black, setBlack] = useState({
-    moves: [],
-  });
-
-  // check game's states
-  useEffect(() => {
-    if (game.dice.length == 2) {
-      toast(game.dice[0]);
-      toast(game.dice[1]);
-    }
-  }, [game.dice]);
-
-  // check white player's states
-  useEffect(() => {
-    if (white.moves.length == 2) {
-      checkState(white.moves[0], white.moves[1]);
-    }
-  }, [white.moves]);
-
-  // check black player's states
-  useEffect(() => {
-    if (black.moves.length == 2) {
-      checkState(black.moves[0], black.moves[1]);
-    }
-  }, [black.moves]);
 
   // roll dice function, it will generate two random numbers between 1 and 6
   function rollDice() {
     const first = Math.floor(Math.random() * 6) + 1;
     const second = Math.floor(Math.random() * 6) + 1;
-    let temp = { ...game };
-    temp.dice = [first, second];
-    setGame(temp);
+    let dices = [first, second];
+    if (first == second) {
+      dices.push(first);
+      dices.push(second);
+    }
+
+    setDice(dices);
   }
 
-  function checkState(from, to) {
-    let player = white;
-    if (game.turn == -1) {
-      player = black;
-    }
-    let temp = { ...game };
-    let thePiece = temp.board[from].pop();
-    temp.board[to].push(thePiece);
-    setGame(temp);
-    player.moves = [];
+  function calculateAvailableMoves() {
+    const from = player.selectedBars[0];
+    let dest1 = from - dice[0];
+    let dest2 = from - dice[1];
+    let dest3 = from - (dice[0] + dice[1]);
+    if (turn == 1) {
+      if (dest1 < 0) dest1 = 11 - dest1;
 
-    // const currBoard = [...board];
-    // // TODO
-    // setBoard(currBoard);
+      if (dest2 < 0) dest2 = 11 - dest2;
+
+      if (dest3 < 0) dest3 = 11 - dest3;
+    } else {
+      if (dest1 < 12) dest1 = 11 - dest1;
+
+      if (dest2 < 12) dest2 = 11 - dest2;
+
+      if (dest3 < 12) dest3 = 11 - dest3;
+    }
+
+    const destinations = [dest1, dest2, dest3];
+    const available = [];
+    let color = "white";
+    if (turn == -1) color = "black";
+
+    destinations.forEach((dest) => {
+      if (!isNaN(dest)) {
+        if (board[dest].top() == color || board[dest].top() == undefined) {
+          available.push(dest);
+        } else if (board[dest].length() == 1) {
+          available.push(dest);
+        }
+      }
+    });
+    let temp = { ...player };
+    temp.availableMoves = available;
+    setPlayer(temp);
+  }
+
+  function movement() {
+    const from = player.selectedBars[0];
+    const to = player.selectedBars[1];
+    const moves = player.availableMoves;
+    // check if from and to are the same, check if the destination is available for the player
+    if (from == to) {
+      toast("Canceled");
+    } else if (!moves.includes(to)) {
+      toast.error("You can't do this");
+    } else {
+      // update the board
+      let thePiece = board[from].pop();
+      board[to].push(thePiece);
+
+      // update player
+      let p = { ...player };
+
+      p.availableMoves.pop();
+      const index = p.availableMoves.indexOf(to);
+      if (index > -1) {
+        p.availableMoves.splice(index, 1);
+      }
+      setPlayer(p);
+
+      // update dice
+      let d = [...dice];
+      const diceNum = from - to;
+      const din = d.indexOf(diceNum);
+      if (din > -1) {
+        d.splice(index, 1);
+      }
+      setDice(d);
+      if (d == 0) {
+        setTurn((prev) => prev * -1);
+      }
+    }
+
+    let temp = { ...player };
+    temp.selectedBars = [];
+    setPlayer(temp);
   }
 
   // select bar function, it will select the bar player clicked on
   function select(index) {
-    let player = white;
-    if (game.turn == -1) {
-      player = black;
-    }
+    // check if dice has been rolled
+    if (dice.length == 0) return toast.error("You must roll the dice first!");
+
+    // check for the right player
+    if (
+      (turn == 1 &&
+        board[index].top() == "black" &&
+        player.selectedBars.length == 0) ||
+      (turn == -1 &&
+        board[index].top() == "white" &&
+        player.selectedBars.length == 0)
+    )
+      return toast.error("It's not your turn!");
+
     // check if the first selected bar is empty
-    if (game.board[index].top() == undefined && player.moves.length == 0) {
-      toast.error("There Is No Piece In This Place");
-    } else {
-      let temp = { ...player };
-      temp.moves = [...player.moves, index];
-      if (player == white) {
-        setWhite(temp);
-      } else {
-        setBlack(temp);
-      }
-    }
+    if (board[index].top() === undefined && player.selectedBars.length == 0)
+      return toast.error("There Is No Piece In This Place");
+
+    let temp = { ...player };
+    temp.selectedBars.push(index);
+    setPlayer(temp);
+
+    if (player.selectedBars.length == 1) calculateAvailableMoves();
+
+    if (player.selectedBars.length == 2) movement();
   }
 
   return (
     <>
       <Board>
-        {game.board.map((bar, barIdx) => (
+        {board.map((bar, barIdx) => (
           <Bar
             isTopRow={barIdx > 11}
             onClick={() => select(barIdx)}
@@ -106,6 +156,9 @@ function App() {
         ))}
       </Board>
       <button onClick={rollDice}>🎲 Roll dice 🎲</button>
+      <div className="dice">
+        {dice.length > 0 ? `Dice Numbers: ${dice[0]} , ${dice[1]}` : ""}
+      </div>
     </>
   );
 }
